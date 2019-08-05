d2a40dff62e399d3204716ee20f463280b963bb7
Nikhil <nikhil.dhage@gmail.com>
2019-08-04 22:37:02 -0400

    changed application to log number of drawn Objects

--- Lumious-core/main.cpp
+++ Lumious-core/main.cpp
@@ -1,15 +1,17 @@
 #pragma once
 
 
-#include "src/graphics/window.h" 
+#include "src/graphics/window.h"
 #include "src/graphics/shader.h"
+#include "src/utils/timer.h"
 #include "src/maths/maths.h"
 #include "src/graphics/rectangle.h"
-#include "src/utils/timer.h"
 #include "src/graphics/layers/tilelayer.h"
 #include "src/graphics/layers\group.h"
 #include "src/graphics/Color.h"
 #include <iostream>
+
+
 
 
 using namespace Lumious;
@@ -18,54 +20,51 @@ using namespace maths;
 
 int main(int argc, char* argv[] ) 
 {
-	 //creating window
-
-	Window window("Lumious!", 960, 540);
-
-
-	//glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-   //std::cout << glGetString(GL_VERSION) << std::endl;
-
+	Timer time;
+	float timer = 0;
+	unsigned int frames = 0;
+	int renderables = 0;
 	
-
-// creating shader and reading from shader files 
+	//creating window
+	Window window ("Lumious",900  ,540);
+	
+	//creating shader and reading from shader files 
 	Shader& shader = *new Shader("res/shaders/basic.vert", "res/shaders/basic.frag");;
 	Shader& shader2 = *new Shader("res/shaders/basic.vert", "res/shaders/basic.frag");;
 
-
+	vec2 aspectRatio(16.0f, 9.0f);
 	TileLayer layer(&shader);
 	
 	TileLayer layer2(&shader2);
 	
-	Rect& s2 = *new Rect(2, 2, 2.0f, 2.0f, Color::Red);
-	
-	for ( float y=0; y < 9.0f; y+= 0.1f)
+	for(float y=0; y < aspectRatio.y; y+=0.05f)
 	{
-		for (float x=0; x < 16.0f; x +=0.1f)
+
+		for (float x = 0; x < aspectRatio.x; x += 0.05f)
 		{
-			 Rect& s = *new Rect(x, y, 0.09f, 0.09f, Color::Aqua );
-			layer.add(&s);
+			Rect* s3 = new Rect(x, y, 0.04f, 0.04f, Color::Fushhia);
+			layer.add(s3);
 		}
-		
 	}
+
+	Rect* s2 = new Rect(2, 2, 2.0f, 2.0f, Color::Green);
+	layer2.add(s2);
+
 	
-	layer2.add(&s2);
 	
 	
 	
 
 	//create Timer
-	Timer time;
-	float timer = 0;
-	unsigned int frames = 0;
-	
+	renderables = layer.getSize()+ layer2.getSize();
 	//game loop
 	while (!window.closed())
 	{
-
+		
 		//Start Renderering
 		//clearing the window
 		window.clear();
+		
 		
 		
 		/*mat4 mat = mat4::translation(vec3(5, 5, 5));
@@ -100,42 +99,16 @@ int main(int argc, char* argv[] )
 		layer.render();
 		layer2.render();
 		
-		
-		/*
-		#if BATCH_RENDERER
-				renderer.begin();
-		#endif
-				// submit Sprites
-			   for (size_t i = 0; i < sprites.size(); i++)
-			   {
-
-				   renderer.submit(sprites[i]);
-				   //printf("%d sprites\n" , sprites[i]);
-			   }
-
-
-		#if BATCH_RENDERER
-			   //Stop Renderering
-			   renderer.end();
-
-		#endif
-
-			   //flush and clean opengl glMapBuffer
-				renderer.flush();
-				*/
-
-		
-	   //updating the window every frame
+		//updating the window every frame
 		window.update();
-
 		
-
 		frames++;
 		if (time.elapsed() - timer >= 1.0f)
 		{
 
 			timer += 1.0f;
 			printf("%d fps\n", frames);
+			printf("%d Renderables\n", renderables);
 			frames = 0;
 		} 
 
@@ -143,62 +116,9 @@ int main(int argc, char* argv[] )
 		// keyboard imput for Key A
 		
 
-		if (window.isKeyPressed(GLFW_KEY_A))
-		{
-
-			std::cout << " A" << std::endl;
-		}
-
-		if (window.isKeyPressed(GLFW_KEY_S))
-		{
-			std::cout << " S" << std::endl;
-		}
-		if (window.isKeyTyped(GLFW_KEY_R))
-		{
-			std::cout << "Rotating Square" << std::endl;
-			mat4 transform =maths::mat4::translation(vec3(5, 5, 0));
-			layer2.getRenderer()->push(transform);
-			std::string  str = "nikhil";
-			std::cout << str << std::endl;
-		}
-
-		if (window.isKeyPressed(GLFW_KEY_LEFT))
-		{
-			std::cout << "Left";
-			mat4 transform = maths::mat4::translation(vec3(-1, 0, 0));
-			layer2.getRenderer()->push(transform);
-		}
-
-		if (window.isKeyPressed(GLFW_KEY_RIGHT))
-		{
-			mat4 transform = maths::mat4::translation(vec3(1, 0, 0));
-			layer2.getRenderer()->push(transform);
-		}
-
-		if (window.isKeyPressed(GLFW_KEY_D))
-		{
-
-			std::cout << " D" << std::endl;
-		}
-
-		if (window.isKeyTyped(GLFW_KEY_W))
-		{
-			
-		}
-
-
-		if (window.isMouseButtonClicked(GLFW_MOUSE_BUTTON_1))
-		{
-			std::cout << " Clicked" << std::endl;
-		}
-
-		//keyboard imput for Key ESCAPE
-		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_RIGHT))
-		{
-			std::cout << " Pressed" << std::endl;
-		}
+		
 	}
-
+	
 return 0;
 }
 
